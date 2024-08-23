7e5a3604bf17cadce31b6c86acb5f3f704db08b7
pablogutierrezbci <pablo.gutierrez@bci.cl>
2024-08-22 21:23:45 -0400

    asd

--- Codigos/MM_poo.py
+++ Codigos/MM_poo.py
@@ -5,96 +5,21 @@ import warnings
 import networkx as nx
 import time
 warnings.filterwarnings("ignore")
-
 from Source.MathematicalModel import MathematicalModel as MILP
-
-
-def integer_fractional_cut1(modelo:gp.Model, donde):
-    initial = time.time()
-    n = modelo._n
-    if donde == GRB.Callback.MIPNODE  and ( modelo.cbGet(GRB.Callback.MIPNODE_STATUS) == GRB.OPTIMAL):
-        valoresX = modelo.cbGetNodeRel(modelo._xvars)
-        tour = [i for i in range(n+1)]
-        MILP.subtour_method(tour, valoresX,n)
-        if len(tour) < n:
-            modelo._callback_count +=1 
-            tour2 = [i for i in range(n) if i not in tour]
-            modelo.cbLazy(gp.quicksum(modelo._xvars[i, j] for i in tour for j in tour2) >= 1)
-            modelo.cbLazy(gp.quicksum(modelo._xvars[i, j] for i in tour for j in tour if i != j) <= len(tour)-1)    
-    modelo._callback_time += time.time()-initial
-
-def integer_fractional_cut2(modelo:gp.Model, donde):
-    initial = time.time()
-    n = modelo._n
-    if donde == GRB.Callback.MIPNODE  and ( modelo.cbGet(GRB.Callback.MIPNODE_STATUS) == GRB.OPTIMAL):
-        valoresX = modelo.cbGetNodeRel(modelo._xvars)
-        tour = [i for i in range(n+1)]
-        MILP.subtour_method(tour, valoresX,n)
-        if len(tour) < n:
-            modelo._callback_count +=1 
-            tour2 = [i for i in range(n) if i not in tour]
-            modelo.cbLazy(gp.quicksum(modelo._xvars[i, j] for i in tour for j in tour2) >= 1)
-            #modelo.cbLazy(gp.quicksum(modelo._xvars[i, j] for i in tour for j in tour if i != j) <= len(tour)-1)    
-
-        
-        DG: nx.DiGraph = modelo._DG
-        for i,j in DG.edges:
-            DG.edges[i,j]['capacity'] = valoresX[i,j]
-
-        for t in range(1,n):
-            (cut_value, node_partition) = nx.minimum_cut( DG, _s=0, _t=t )
-            # print("cut_value =",cut_value)
-            if cut_value < 1 - modelo._epsilon:
-                modelo._callback_count +=1 
-                S = node_partition[0]  # 'left' side of the cut
-                T = node_partition[1]  # 'right' side of the cut
-                modelo.cbLazy( gp.quicksum( modelo._xvars[i,j] for i in S for j in T ) >= 1 )
-                return
-
-    modelo._callback_time += time.time()-initial
-
-def integer_fractional_cut3(modelo:gp.Model, donde):
-    initial = time.time()
-    n = modelo._n
-    if donde == GRB.Callback.MIPNODE  and ( modelo.cbGet(GRB.Callback.MIPNODE_STATUS) == GRB.OPTIMAL):
-        valoresX = modelo.cbGetNodeRel(modelo._xvars)
-        tour = [i for i in range(n+1)]
-        MILP.subtour_method(tour, valoresX,n)
-        if len(tour) < n:
-            modelo._callback_count +=1 
-            tour2 = [i for i in range(n) if i not in tour]
-            modelo.cbLazy(gp.quicksum(modelo._xvars[i, j] for i in tour for j in tour2) >= 1)
-            modelo.cbLazy(gp.quicksum(modelo._xvars[i, j] for i in tour for j in tour if i != j) <= len(tour)-1)    
-
-        DG: nx.DiGraph = modelo._DG
-        for i,j in DG.edges:
-            DG.edges[i,j]['capacity'] = valoresX[i,j]
-
-        for t in range(1,n):
-            (cut_value, node_partition) = nx.minimum_cut( DG, _s=0, _t=t )
-            # print("cut_value =",cut_value)
-            if cut_value < 1 - modelo._epsilon:
-                modelo._callback_count +=1 
-                S = node_partition[0]  # 'left' side of the cut
-                T = node_partition[1]  # 'right' side of the cut
-                modelo.cbLazy( gp.quicksum( modelo._xvars[i,j] for i in S for j in T ) >= 1 )
-                return
-
-    modelo._callback_time += time.time()-initial
 
 argv = sys.argv[1:]
 opts = [(argv[2*i],argv[2*i+1]) for i in range(int(len(argv)/2))]
+size = "transitional"
+instance = 1
 
-size = "tsplib"
-instance = "gr17"
-output = False
-subtour = "gg"
-initial_sol = False
-callback =  'None'
-bounds = False
-new_formulation = False
-time_limit = 3600
-new_m = False
+output = True
+subtour = "wc"
+initial_sol = True
+callback =  'both_blossom2'
+bounds = True
+new_formulation = True
+time_limit = 2
+new_m = True
 relax = False
 
 for i in range(len(opts)):
@@ -104,19 +29,11 @@ for i in range(len(opts)):
             instance = int(opts[i][1])  
         except ValueError:
             instance = opts[i][1]  
-    elif   opts[i][0][1:] == "initial_sol": initial_sol = False if opts[i][1].lower() == "false" else True
+    elif   opts[i][0][1:] == "initialsol": initial_sol = False if opts[i][1].lower() == "false" else True
     elif   opts[i][0][1:] == "output": output =  False if opts[i][1].lower() == "false" else True
     elif   opts[i][0][1:] == "bounds": bounds = False if opts[i][1].lower() == "false" else True
     elif   opts[i][0][1:] == "subtour": subtour = opts[i][1]
-    elif   opts[i][0][1:] == "callback": 
-        if opts[i][1] == "custom1":
-            callback = integer_fractional_cut1
-        elif opts[i][1] == "custom2":
-            callback = integer_fractional_cut2
-        elif opts[i][1] == "custom3":
-            callback = integer_fractional_cut3
-        else:
-            callback = opts[i][1]
+    elif   opts[i][0][1:] == "callback": callback = opts[i][1]
     elif   opts[i][0][1:] == "newformulation": new_formulation =  False if opts[i][1].lower() == "false" else True
     elif   opts[i][0][1:] == "newm": new_m =  False if opts[i][1].lower() == "false" else True
     elif   opts[i][0][1:] == "relax": relax =  False if opts[i][1].lower() == "false" else True
@@ -134,8 +51,7 @@ MM = MILP(size,
           relax = relax
           )
 MM.run()
-
-solution = {key:value for key,value in MM.modelo.getAttr('x', MM.x).items() if value>0.1}
+# solution = {key:value for key,value in MM.modelo.getAttr('x', MM.x).items() if value>0.1}
 # print(MM.get_solution()[0])
 MM.print_results()
 
